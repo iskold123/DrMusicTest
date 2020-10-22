c5b68d4d87e89cf09fa250cf67b69ad3b185b232
Mads Olsen <mads36g6@edu.easj.dk>
2020-10-22 12:45:37 +0200

    ,,,,

--- src/js/index.ts
+++ src/js/index.ts
@@ -3,16 +3,16 @@ import axios, {
     AxiosError
 } from "../../node_modules/axios/index"
 
-interface IBook {
-    id: number
+interface IMusic {
     title: string
-    author: string
-    publisher: string
-    price: number
+    artist: string
+    duration: number
+    yearOfPublication: number
 }
 
-let baseUrl: string = "http://anbo-bookstorerest.azurewebsites.net/api/books"
 
+let baseUrl: string = "http://musicservice.azurewebsites.net/api/Musics"
+//let baseUrl: string = "http://localhost:51068/api/Musics"
 
 new Vue({
     // TypeScript compiler complains about Vue because the CDN link to Vue is in the html file.
@@ -20,87 +20,13 @@ new Vue({
     // which is included at the bottom of the html file.
     el: "#app",
     data: {
-        name: "",
-        greeting: "",
-        books: [],
-        id: "",
-        book: null,
-        inputData: { title: "", author: "", publisher: "", price: 0 },
-        addMessage: "",
-        idToDelete: "",
-        deleteMessage: "",
-        idToUpdate: null,
-        updateData: { title: "", author: "", publisher: "", price: 0 },
-        updateMessage: ""
+        records: [],
+        
     },
-    created(): void {
-        console.log("created")
-        this.getAndShowAllBooks();
-    },
+
+
     methods: {
-        getAndShowAllBooks(): void {
-            axios.get<IBook[]>(baseUrl)
-                .then((response: AxiosResponse<IBook[]>) => {
-                    this.books = response.data
-                })
-                .catch((error: AxiosError) => {
-                    alert(error.message)
-                })
-        },
-        getBookById(id: number): void {
-            let uri: string = baseUrl + "/" + id
-            console.log("getBookById: " + uri)
-            axios.get<IBook>(uri)
-                .then((response: AxiosResponse<IBook>) => {
-                    this.book = response.data
-                })
-                .catch((error: AxiosError) => {
-                    alert(error.message)
-                })
-        },
-        addBook(): void {
-            console.log("addBook")
-            axios.post<number>(baseUrl, this.inputData)
-                .then((response: AxiosResponse<number>) => {
-                    this.addMessage = "Book added"
-                    this.getAndShowAllBooks()
-                })
-                .catch((error: AxiosError) => {
-                    alert(error.message)
-                })
-        },
-        deleteBookById(id: number): void {
-            let uri: string = baseUrl + "/" + id
-            console.log("deleteBookById " + uri)
-            axios.delete<number>(uri)
-                .then((response: AxiosResponse<number>) => {
-                    console.log("deleteBookById result " + response.data)
-                    if (response.data == 1) {
-                        this.deleteMessage = "Book deleted"
-                        this.getAndShowAllBooks()
-                    } else {
-                        this.deleteMessage = "No such book"
-                    }
-                })
-                .catch((error: AxiosError) => {
-                    alert(error.message)
-                })
-        },
-        updateBook(id: number): void {
-            let uri: string = baseUrl + "/" + id
-            console.log("update book " + uri)
-            axios.put<number>(uri, this.updateData)
-                .then((response: AxiosResponse<number>) => {
-                    if (response.data == 1) {
-                        this.updateMessage = "Book updated"
-                        this.getAndShowAllBooks()
-                    } else {
-                        this.updateMessage = "No such book"
-                    }
-                })
-                .catch((error: AxiosError) => {
-                    alert(error.message)
-                })
-        }
-    }
+        
+    },
+       
 })
