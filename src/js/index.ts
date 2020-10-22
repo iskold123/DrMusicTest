e775a250170862d523e6c2540da34484e6b0e29c
Mads Olsen <mads36g6@edu.easj.dk>
2020-10-22 13:53:38 +0200

    ...

--- src/js/index.ts
+++ src/js/index.ts
@@ -21,12 +21,84 @@ new Vue({
     el: "#app",
     data: {
         records: [],
-        
+        // id: "",
+        // music: null,
+        // inputData: { title: "", artist: "", duration: "", yearOfPublication: 0 },
+        // addMessage: "",
+        // idToDelete: "",
+        // deleteMessage: "",
+        // idToUpdate: null,
+        // updateData: { title: "", artist: "", duration: "", yearOfPublication: 0 },
+        // updateMessage: ""
     },
-
-
+    created(): void {
+        console.log("created")
+        this.getAndShowAllrecords();
+    },
     methods: {
-        
-    },
-       
+        getAndShowAllrecords(): void {
+            axios.get<IMusic[]>(baseUrl)
+                .then((response: AxiosResponse<IMusic[]>) => {
+                    this.records = response.data
+                })
+                .catch((error: AxiosError) => {
+                    alert(error.message)
+                })
+        },
+        getBookById(id: number): void {
+            let uri: string = baseUrl + "/" + id
+            console.log("getBookById: " + uri)
+            axios.get<IBook>(uri)
+                .then((response: AxiosResponse<IBook>) => {
+                    this.book = response.data
+                })
+                .catch((error: AxiosError) => {
+                    alert(error.message)
+                })
+        },
+        addBook(): void {
+            console.log("addBook")
+            axios.post<number>(baseUrl, this.inputData)
+                .then((response: AxiosResponse<number>) => {
+                    this.addMessage = "Book added"
+                    this.getAndShowAllBooks()
+                })
+                .catch((error: AxiosError) => {
+                    alert(error.message)
+                })
+        },
+        deleteBookById(id: number): void {
+            let uri: string = baseUrl + "/" + id
+            console.log("deleteBookById " + uri)
+            axios.delete<number>(uri)
+                .then((response: AxiosResponse<number>) => {
+                    console.log("deleteBookById result " + response.data)
+                    if (response.data == 1) {
+                        this.deleteMessage = "Book deleted"
+                        this.getAndShowAllBooks()
+                    } else {
+                        this.deleteMessage = "No such book"
+                    }
+                })
+                .catch((error: AxiosError) => {
+                    alert(error.message)
+                })
+        },
+        updateBook(id: number): void {
+            let uri: string = baseUrl + "/" + id
+            console.log("update book " + uri)
+            axios.put<number>(uri, this.updateData)
+                .then((response: AxiosResponse<number>) => {
+                    if (response.data == 1) {
+                        this.updateMessage = "Book updated"
+                        this.getAndShowAllBooks()
+                    } else {
+                        this.updateMessage = "No such book"
+                    }
+                })
+                .catch((error: AxiosError) => {
+                    alert(error.message)
+                })
+        }
+    }
 })
